7d0de54867953efb7f08d867a84f1bc35c45ed8d
Alex Rogozhnikov <iamfullofspam@gmail.com>
2016-07-16 03:55:47 +0200

    not deleting sources in conda

--- ci/install_repbase.sh
+++ ci/install_repbase.sh
@@ -79,7 +79,7 @@ source activate jupyterhub_py3 || halt "Error installing jupyterhub_py3 environm
 
 echo "Removing conda packages and caches"
 conda uninstall --force --yes -q gcc qt
-conda clean --yes -s -p -l -i -t
+conda clean --yes -p -l -i -t
 
 
 echo "Creating conda venv $REP_ENV_NAME"
